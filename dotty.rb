e21bdc69ef7bdeea8af7f9d4d9f0af498c8b78d9
Ian Whitney <whit0694@umn.edu>
2017-03-21 20:46:41 -0500

    Update sha.

--- dotty.rb
+++ dotty.rb
@@ -3,7 +3,7 @@ class Dotty < Formula
   homepage "https://github.umn.edu/asrweb/crystal_dotty"
   url "https://github.umn.edu/asrweb/crystal_dotty/releases/download/v0.1.0/dotty-0.1.0.osx.zip"
   version "0.1.0"
-  sha256 "042095a08948c61fd475aed0aa693c8ef1dc06be7267d6a9a3fcc61f39160b52"
+  sha256 "ad75c48a8eb6bbabb61b22f4eb2681f8217e1d845c9307db274a0b3bfcdbc2b3"
 
   def install
     bin.install buildpath/"dotty"
