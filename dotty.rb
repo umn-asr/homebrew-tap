18d08b0ae5857f91136d259490d53a2d90d3fa49
Ian Whitney <whit0694@umn.edu>
2017-03-22 14:10:38 -0500

    Convert to rust

--- dotty.rb
+++ dotty.rb
@@ -1,15 +1,15 @@
 class Dotty < Formula
   desc "Configuration and Dotfile manager for ASR Custom Solutions"
-  homepage "https://github.com/umn-asr/crystal_dotty"
-  url "https://github.com/umn-asr/crystal_dotty/releases/download/v0.1.0/dotty-0.1.0.osx.zip"
+  homepage "https://github.com/umn-asr/rust_dotty"
+  url "https://github.com/umn-asr/rust_dotty/releases/download/v0.1.0/rust_dotty-0.1.0.osx.zip"
   version "0.1.0"
-  sha256 "042095a08948c61fd475aed0aa693c8ef1dc06be7267d6a9a3fcc61f39160b52"
+  sha256 "2eec7fd60fce94a2aa310672d9543944388142fa723bf4e5d02525dbe77a0ce1"
 
   def install
-    bin.install buildpath/"dotty"
+    bin.install buildpath/"rust_dotty"
   end
 
   test do
-    system "#{bin}/dotty --help"
+    system "#{bin}/rust_dotty --help"
   end
 end
