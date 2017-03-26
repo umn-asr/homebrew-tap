8d26978f55990eb327f4f0f06244894f047c09e2
Ian Whitney <whit0694@umn.edu>
2017-03-26 15:42:58 -0500

    Release reekup 0.2.0

--- reekup.rb
+++ reekup.rb
@@ -1,15 +1,15 @@
 class Reekup < Formula
   desc "Configures a project's reek settings"
   homepage "https://github.com/umn-asr/reekup"
-  url "https://github.com/umn-asr/reekup/releases/download/0.1.0/reekup-0.1.0.osx.zip"
-  version "0.1.0"
-  sha256 "5cb37f794b5cd4f3ab2319613c83505f1b475a3f0a299ef0bf1dfcbf8c4613c6"
+  url "https://github.com/umn-asr/reekup/releases/download/0.2.0/reekup-0.2.0.osx.zip"
+  version "0.2.0"
+  sha256 "f08709a77d66929675ad1549c0cdef17ef7f48a786f2b4e3993b21db865fcc82"
 
   def install
     bin.install buildpath/"reekup"
   end
 
   test do
-    # noop
+    system "reekup -h"
   end
 end
